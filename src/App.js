fc16e7ed6bb0b22f5a6492e7e9e1333e3688c49c
Alejadro Sanchez-Giraldo <morsisdivine@gmail.com>
2024-09-03 21:25:21 +1000

    add complex equations

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { getBrowserInfo } from './browserInfo';
 import './App.css';
 
 function App() {
-  const { showNameInput } = useFlags();
+  const { showNameInput, advanceFunctions } = useFlags();
   const ldClient = useLDClient();
   const [name, setName] = useState('');
   const [input, setInput] = useState('');
@@ -66,19 +66,25 @@ function App() {
             <button onClick={() => handleClick('/')}>/</button>
             <button onClick={() => handleClick('*')}>*</button>
             <button onClick={() => handleClick('-')}>-</button>
+            
             <button onClick={() => handleClick('7')}>7</button>
             <button onClick={() => handleClick('8')}>8</button>
             <button onClick={() => handleClick('9')}>9</button>
             <button onClick={() => handleClick('+')}>+</button>
+
             <button onClick={() => handleClick('4')}>4</button>
             <button onClick={() => handleClick('5')}>5</button>
             <button onClick={() => handleClick('6')}>6</button>
             <button onClick={handleCalculate}>=</button>
+
             <button onClick={() => handleClick('1')}>1</button>
             <button onClick={() => handleClick('2')}>2</button>
             <button onClick={() => handleClick('3')}>3</button>
+            {advanceFunctions && <button onClick={() => handleClick('(')}>(</button>}
+
             <button className="zero" onClick={() => handleClick('0')}>0</button>
             <button onClick={() => handleClick('.')}>.</button>
+            {advanceFunctions && <button onClick={() => handleClick(')')}>)</button>}
           </div>
         </div>
       </div>
