c9a7a06742ed41f467b35b0a9ce26081e5d9dcd7
Alejadro Sanchez-Giraldo <morsisdivine@gmail.com>
2024-09-05 17:44:26 +1000

    create classic Calc module

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useFlags, useLDClient } from 'launchdarkly-react-client-sdk';
-import NameInput from './NameInput';
+import NameInput from './modules/NameInput';
 import { getBrowserInfo } from './browserInfo';
+import ClassicCalculator from './modules/ClassicCalculator';
 
 import './App.css';
 
 function App() {
-  const { showNameInput, advanceFunctions } = useFlags();
+  const { showNameInput, advanceFunctions, calculatorStyle } = useFlags();
   const ldClient = useLDClient();
   const [name, setName] = useState('');
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
-
   const handleNameSubmit = (name) => {
     setName(name);
   };
@@ -52,45 +52,54 @@ function App() {
     }
   };
 
-  return (
-    <div>
-      {showNameInput && <h1 className="welcome-title">Welcome, {name}!</h1>}
+  // Display Classic Calculator if classicCalc flag is true
+  if (calculatorStyle) {
+    return (
       <div className="App">
-        <div className="calculator">
-          <div className="display">
-            <div className="input">{input}</div>
-            <div className="result">{result}</div>
-          </div>
-          <div className="buttons">
-            <button onClick={handleClear}>C</button>
-            <button onClick={() => handleClick('/')}>/</button>
-            <button onClick={() => handleClick('*')}>*</button>
-            <button onClick={() => handleClick('-')}>-</button>
+        <ClassicCalculator />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {showNameInput && <h1 className="welcome-title">Welcome, {name}!</h1>}
+        <div className="App">
+          <div className="calculator">
+            <div className="display">
+              <div className="input">{input}</div>
+              <div className="result">{result}</div>
+            </div>
+            <div className="buttons">
+              <button onClick={handleClear}>C</button>
+              <button onClick={() => handleClick('/')}>/</button>
+              <button onClick={() => handleClick('*')}>*</button>
+              <button onClick={() => handleClick('-')}>-</button>
 
-            <button onClick={() => handleClick('7')}>7</button>
-            <button onClick={() => handleClick('8')}>8</button>
-            <button onClick={() => handleClick('9')}>9</button>
-            <button onClick={() => handleClick('+')}>+</button>
+              <button onClick={() => handleClick('7')}>7</button>
+              <button onClick={() => handleClick('8')}>8</button>
+              <button onClick={() => handleClick('9')}>9</button>
+              <button onClick={() => handleClick('+')}>+</button>
 
-            <button onClick={() => handleClick('4')}>4</button>
-            <button onClick={() => handleClick('5')}>5</button>
-            <button onClick={() => handleClick('6')}>6</button>
-            <button onClick={handleCalculate}>=</button>
+              <button onClick={() => handleClick('4')}>4</button>
+              <button onClick={() => handleClick('5')}>5</button>
+              <button onClick={() => handleClick('6')}>6</button>
+              <button onClick={handleCalculate}>=</button>
 
-            <button onClick={() => handleClick('1')}>1</button>
-            <button onClick={() => handleClick('2')}>2</button>
-            <button onClick={() => handleClick('3')}>3</button>
-            {advanceFunctions && <button className="special-key" onClick={() => handleClick('(')}>(</button>}
+              <button onClick={() => handleClick('1')}>1</button>
+              <button onClick={() => handleClick('2')}>2</button>
+              <button onClick={() => handleClick('3')}>3</button>
+              {advanceFunctions && <button className="special-key" onClick={() => handleClick('(')}>(</button>}
 
-            <button className="zero" onClick={() => handleClick('0')}>0</button>
-            <button onClick={() => handleClick('.')}>.</button>
-            {advanceFunctions && <button className="special-key" onClick={() => handleClick(')')}>)</button>}
+              <button className="zero" onClick={() => handleClick('0')}>0</button>
+              <button onClick={() => handleClick('.')}>.</button>
+              {advanceFunctions && <button className="special-key" onClick={() => handleClick(')')}>)</button>}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/modules/ClassicCalculator.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useFlags } from 'launchdarkly-react-client-sdk';
+
+
+const ClassicCalculator = () => {
+    const { advanceFunctions } = useFlags();
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState('');
+
+    const handleClick = (value) => {
+        setInput(input + value);
+    };
+
+    const handleClear = () => {
+        setInput('');
+        setResult('');
+      };
+
+    const handleCalculate = () => {
+    try {
+        // eslint-disable-next-line no-eval
+        setResult(eval(input).toString());
+    } catch (error) {
+        setResult('Error');
+    }
+    };
+
+  return (
+    <div>
+      <h1 className="welcome-title">Classic Calculator</h1>
+      <div className="calculator">
+        <div className="display">
+            <div className="input">{input}</div>
+            <div className="result">{result}</div>
+        </div>
+        <div className="buttons">
+            <button onClick={handleClear}>C</button>
+            <button onClick={() => handleClick('/')}>/</button>
+            <button onClick={() => handleClick('*')}>*</button>
+            <button onClick={() => handleClick('-')}>-</button>
+
+            <button onClick={() => handleClick('7')}>7</button>
+            <button onClick={() => handleClick('8')}>8</button>
+            <button onClick={() => handleClick('9')}>9</button>
+            <button onClick={() => handleClick('+')}>+</button>
+
+            <button onClick={() => handleClick('4')}>4</button>
+            <button onClick={() => handleClick('5')}>5</button>
+            <button onClick={() => handleClick('6')}>6</button>
+            <button onClick={handleCalculate}>=</button>
+
+            <button onClick={() => handleClick('1')}>1</button>
+            <button onClick={() => handleClick('2')}>2</button>
+            <button onClick={() => handleClick('3')}>3</button>
+            {advanceFunctions && <button className="special-key" onClick={() => handleClick('(')}>(</button>}
+
+            <button className="zero" onClick={() => handleClick('0')}>0</button>
+            <button onClick={() => handleClick('.')}>.</button>
+            {advanceFunctions && <button className="special-key" onClick={() => handleClick(')')}>)</button>}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClassicCalculator;
